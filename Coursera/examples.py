89453b2b330541118ad9865ff6b328ed4087edbc
Dmitry <122685309+Dragoner595@users.noreply.github.com>
2024-04-22 14:23:40 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	renamed:    Coursera/Regresion.py -> Coursera/examples.py
    commit

--- Coursera/examples.py
+++ Coursera/examples.py
@@ -9,3 +9,16 @@ housing_data=np.random.randint(500,2500,size=(rows,cols-3))
 housing_data = np.column_stack((housing_data, np.random.randint(1, 10, size=(rows,cols-2))))
 housing_data = np.column_stack((housing_data, np.random.randint(10, 50, size=rows)))
 
+# make dot product multiplication 
+def dot_prod(a,b):
+
+    x=0
+
+    for i in range (a.shape[0]):
+        x=x+a[i]*b[i]
+
+    return x
+print(housing_data[:10])
+a=housing_data[:,1]
+b=housing_data[:,2]
+print(f"dot_prod(a,b)={dot_prod(a,b)}")
