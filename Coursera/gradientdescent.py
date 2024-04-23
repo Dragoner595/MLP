99c412889d60038831a06bd1fae39a34e888e02a
Dmitry <122685309+Dragoner595@users.noreply.github.com>
2024-04-23 08:59:15 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   Coursera/gradientdescent.py
    	new file:   Cursera lab files/deeplearning.mplstyle
    	new file:   __pycache__/lab_utils_common.cpython-310.pyc
    commit

--- Coursera/gradientdescent.py
+++ Coursera/gradientdescent.py
@@ -1,4 +1,5 @@
 import math, copy
 import numpy as np
 import matplotlib.pyplot as plt
-
+plt.style.use('/workspaces/MLP/Cursera lab files/deeplearning.mplstyle')
+from  lab_utils_uni import plt_house_x, plt_contour_wgrad, plt_divergence, plt_gradients
