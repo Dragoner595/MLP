11e2f424ac24127734916889fefa547bc9cf9d93
Dmitry <122685309+Dragoner595@users.noreply.github.com>
2024-04-26 09:03:52 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   Coursera/multiple_values_vectorise.py
    commit

--- Coursera/multiple_values_vectorise.py
+++ Coursera/multiple_values_vectorise.py
@@ -36,11 +36,11 @@ def predict_single_loop(x,w,b):
 
 # get a row from our training data 
 x_vec=X_train[0,:]
-print(f"x_vec shape {x_vec.shape} , x_vec value: {x_vec}")
+#print(f"x_vec shape {x_vec.shape} , x_vec value: {x_vec}")
 
 # make a prediction 
 f_wb = predict_single_loop(x_vec, w_init,b_init)
-print(f"f_wb_shape {f_wb.shape}, prediction loop: {f_wb}")
+#print(f"f_wb_shape {f_wb.shape}, prediction loop: {f_wb}")
 
 # will performe same calculation but with dot product function 
 
@@ -61,7 +61,7 @@ def prediction(x,w,b):
 # will use same x_vec from before and create new vector f_wb_dot
 f_wb_dot=prediction(x_vec,w_init,b_init)
 
-print(f"F_wb_vector: {f_wb_dot.shape}, prediction dor porduct : {f_wb_dot}")
+#print(f"F_wb_vector: {f_wb_dot.shape}, prediction dor porduct : {f_wb_dot}")
 
 # compute cost with multiple variables 
 
@@ -88,7 +88,7 @@ def compute_cost (X,y,w,b):
 
 # Compute and display cost using our pre-chosen optimal parameters. 
 cost = compute_cost(X_train, y_train, w_init, b_init)
-print(f'Cost at optimal w : {cost}')
+#print(f'Cost at optimal w : {cost}')
 
 
 # compute gradient with multiple values 
@@ -111,21 +111,88 @@ def compute_gradient(X,y,w,b):
     dj_db=0.
 
     for i in range(m):
-        err=(np.dot(X[i],w)+b)-y[i] # we find X[0] == [ 2104,5,1,45] with calculation of w==[0.39133535,18.75376741,-53.36032453,-26.42131618] substract y[460]
+        err=(np.dot(X[i],w)+b)-y[i] # we find X[0] == [ 2104,5,1,45] with calculation of w==[0.39133535,18.75376741,-53.36032453,-26.42131618] substract y[0]==[460]
+        #print(f"update of err before loop {err}")
         for j in range(n):
-            print(f"update of w before loop {dj_dw}")
+            #print(f"update of w before loop {dj_dw}")
             dj_dw[j]=dj_dw[j]+err*X[i,j] # after we find for X[0] we move to loop for each of the column calculation in a row X[0]
             #it will be at first 0[0] = 0[0] + our calcualtion for X[0] multiply by X[0,0] and record it and do same calculation but for X[0,1] and till X[3,4]
         dj_db=dj_db+err
-        print(f"update of w {dj_dw}")
-    print(f"update of w before substraction of m: {dj_dw}")
+        #print(f"update of err {err}")
+        #print(f"update of w {dj_dw}")
+    #print(f"update of w before substraction of m: {dj_dw}")
     dj_dw=dj_dw/m
     dj_db=dj_db/m
     
 
     return dj_db,dj_dw
 
-#Compute and display gradient 
+# Compute and display gradient 
 tmp_dj_db, tmp_dj_dw = compute_gradient(X_train, y_train, w_init, b_init)
-print(f'dj_db at initial w,b: {tmp_dj_db}')
-print(f'dj_dw at initial w,b: \n {tmp_dj_dw}')
+#print(f'dj_db at initial w,b: {tmp_dj_db}')
+#print(f'dj_dw at initial w,b: \n {tmp_dj_dw}')
+
+
+# Gradient descent with Multiple Varialbles 
+
+def gradient_descent (X,y,w_in,b_in,cost_function,gradient_function,alpha,num_iters):
+  """
+    Performs batch gradient descent to learn w and b. Updates w and b by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      X (ndarray (m,n))   : Data, m examples with n features
+      y (ndarray (m,))    : target values
+      w_in (ndarray (n,)) : initial model parameters  
+      b_in (scalar)       : initial model parameter
+      cost_function       : function to compute cost
+      gradient_function   : function to compute the gradient
+      alpha (float)       : Learning rate
+      num_iters (int)     : number of iterations to run gradient descent
+      
+    Returns:
+      w (ndarray (n,)) : Updated values of parameters 
+      b (scalar)       : Updated value of parameter 
+    """
+  
+  # An array to store cost J nad w's at each iteration primarly for graphic representation 
+  J_hist = []
+  w = copy.deepcopy(w_in) #avoid modifying global w within function 
+  b = b_in
+
+
+  for i in range (num_iters):
+      #calculate the gradient and update the parameter 
+      dj_db,dj_dw = gradient_function(X,y,w,b)  #None
+
+      #Update parameters using w,b, alpha and gradient 
+      w = w - alpha*dj_dw       #None
+      b = b - alpha*dj_db       #None
+
+      #Save cost j at ech iteration 
+      if i <100000:         #prevent resource exhausting
+          J_hist.append (cost_function(X,y,w,b))
+      
+      if i % math.ceil(num_iters/10)==0:
+          print(f'Iteration {i:4d}: Cost {J_hist[-1]:8.2f}')
+  
+  return w,b,J_hist #return final w,b and J_history for graphic
+
+#initialize parameters 
+initial_w = np.zeros_like(w_init)
+initial_b = 0  
+
+# some gradient descent settings 
+iterations = 1000 
+alpha = 5.0e-7
+
+# run gradient descent settings 
+
+w_final,b_final,J_hist = gradient_descent(X_train,y_train,initial_w,initial_b,compute_cost,compute_gradient,alpha,iterations)
+
+print( f'b,w found by gradient descent:{b_final: 0.2f},{w_final}')
+m,_=X_train.shape
+
+for i in range(m):
+    print(f'prediction: {np.dot(X_train[i],w_final)+b_final:0.2f},target value: {y_train[i]}')
+
