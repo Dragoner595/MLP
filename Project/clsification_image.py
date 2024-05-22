68f197fbf7669f0b6da79097e1a5d2656c914b4a
Dmitry <122685309+Dragoner595@users.noreply.github.com>
2024-05-22 14:51:04 +0000

    modified:   Project/clsification_image.py commit

--- Project/clsification_image.py
+++ Project/clsification_image.py
@@ -46,3 +46,23 @@ print("Number of dog images in train folder:",len(os.listdir(train_dogs_dir)))
 print("Number of cat images in validation folder:", len(os.listdir(test_cats_dir)))
 print("Number of dog images in validation folder:", len(os.listdir(test_dogs_dir)))
 
+"""
+The image pixel values range between 0 to 255, thus to normalize the images we have to use ImageDataGenerator class which will genrate batches of normalized input images based out of the train and validation directory. 
+Target size of the image selected as (150,150) but it can be rescaled later. 
+Since this project only deals with only two class (Cat and Dog) of images so the class_mode selected as 'binary' for generating the flow of training and validation images.
+"""
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# All images will be rescaled by ImageGenerator 
+
+train_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen =  ImageDataGenerator(rescale = 1. /255)
+
+# Generating training image with batches of 20 
+# image clasification deals only with cat and dogs so we select class mode binary 
+train_generator = train_datagen.flow_from_directory(train_dir,target_size = (150,150),batch_size = 20, class_mode = 'binary')
+
+# Copy same code jsut change to test sample 
+test_generator = test_datagen.flow_from_directory(test_dir,target_size = (150,150),batch_size = 20, class_mode = 'binary')
+
