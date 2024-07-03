6d35d3d87a7ea4bce9676814c2ff25d9124e6398
Dmitry <122685309+Dragoner595@users.noreply.github.com>
2024-07-03 18:04:27 +0000

    modified:   Project/fraud_detection.py commit

--- Project/fraud_detection.py
+++ Project/fraud_detection.py
@@ -43,9 +43,7 @@ This case requires special attention when training or when evaluating the qualit
 One way of handing this case at train time is to bias the model to pay more attention to the samples in the minority class. 
 The models under the current study will be configured to take into account the class weights of the samples at train/fit time.
 '''
-print("Minimal values of the data\n" , big_raw_data.min(),
-      "Maximum values of the data\n" , big_raw_data.max(),
-      "90 percent percentile of the data set\n",np.percentile(raw_data.Amount.values,90))
+#print("Minimal values of the data\n" , big_raw_data.min(),"Maximum values of the data\n" , big_raw_data.max(),"90 percent percentile of the data set\n",np.percentile(raw_data.Amount.values,90))
 
 
 big_raw_data.iloc[:,1:30] = StandardScaler().fit_transform(big_raw_data.iloc[:,1:30])
@@ -66,11 +64,104 @@ X = normalize (X , norm = 'l1')
 
 # print the shape of the features matrix and the labels vector 
 
-print("X.shape=", X.shape , "y.shape=" , y.shape)
+#print("X.shape=", X.shape , "y.shape=" , y.shape)
 
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3, random_state=42,stratify=y)
 
-print("X_train.shape =" , X_train.shape ,'Y_train.shape=',y_train.shape)
-print("X_test.shape=", X_test.shape,"y_test.shape",y_test.shape)
+#print("X_train.shape =" , X_train.shape ,'Y_train.shape=',y_train.shape)
+#print("X_test.shape=", X_test.shape,"y_test.shape",y_test.shape)
 
+
+# It takes into account the class imbalance present in this dataset 
+# we finding needed weight for our prediction model 
+w_train = compute_sample_weight('balanced',y_train) 
+
+#import the dicision tree Classifier model from scikit learn 
+
+from sklearn.tree import DecisionTreeClassifier 
+
+# for reproducible output across multiple function calls ,set random_state to a given integer value 
+
+sklearn_dt = DecisionTreeClassifier(max_depth = 4,random_state= 35 )
+
+# train a dicision tree Clasifier using scihit-learn 
+t0 = time.time() # print ting us training time spend for training 
+
+sklearn_dt.fit(X_train,y_train,sample_weight = w_train)
+
+sklearn_time = time.time()-t0
+#print("[Scikit-Learn] Training time (s):  {0:.5f}".format(sklearn_time))
+
+"""
+if not already computed, 
+compute the sample weights to be used as input to the train routine so that 
+it takes into account the class imbalance present in this dataset
+w_train = compute_sample_weight('balanced', y_train)
+"""
+
+# importing the Decision Tree Classifier Model from Snap ML
+from snapml import DecisionTreeClassifier 
+
+"""
+Snap ML offers multi-threaded CPU/GPU training of decision trees, unlike scikit-learn
+to use the GPU, set the use_gpu parameter to True 
+snapml_dt = DecisionTreeClassifier(max_depth=4, random_state=45, use_gpu=True)
+"""
+
+
+# to set the number of CPU threads used at training time, set the n_jobs parameter
+# for reproducible output across multiple function calls, set random_state to a given integer value
+
+snapml_dt = DecisionTreeClassifier(max_depth=4, random_state=45, n_jobs=4)
+
+#train dicision tree clasifier model using SNap ml
+t0 = time.time()
+
+snapml_dt.fit(X_train,y_train,sample_weight = w_train)
+snapml_time = time.time()-t0
+#print("[Snap ML] Training time (s):  {0:.5f}".format(snapml_time))
+
+training_speedup = sklearn_time/snapml_time
+
+#print('[Decision Tree Classifier] Snap ML vs Scikit-Learn speedup: {0:.2f}x'.format(training_speedup))
+
+# run inference and compute the probabilities of the test samples 
+# to belong to the class of fraudulent transactions
+
+sklearn_pred = sklearn_dt.predict_proba(X_test)[:,1]
+
+# evaluate the Compute Area Under the Receiver Operating Characteristic 
+# Curve (ROC-AUC) score from the predictions
+
+sklearn_roc_auc = roc_auc_score(y_test, sklearn_pred)
+
+print('[Scikit-Learn] ROC-AUC score : {0:.3f}'.format(sklearn_roc_auc))
+
+# run inference and compute the probabilities of the test samples
+# to belong to the class of fraudulent transactions
+
+snapml_pred = snapml_dt.predict_proba(X_test)[:,1]
+
+# evaluate the Compute Area Under the Receiver Operating Characteristic
+# Curve (ROC-AUC) score from the prediction scores
+
+snapml_roc_auc = roc_auc_score(y_test, snapml_pred)   
+print('[Snap ML] ROC-AUC score : {0:.3f}'.format(snapml_roc_auc))
+
+# import the linear Support Vector Machine (SVM) model from Scikit-Learn
+
+from sklearn.svm import LinearSVC
+
+# instantive a scikit-learn  SVM Model 
+# to indicate the class imbalance at fit time , set class_weight = 'balance'
+# for reproducible output across multiple function calls, set random_state to a given integer values 
+sklearn_svm = LinearSVC(class_weight = 'balanced', random_state = 31 , loss = 'highe', fit_intercept = False )
+
+# train a linear Support Vector Machine model using Scikit-Learn 
+
+t0 = time.time()
+sklearn_svm.fit(X_train,y_train)
+sklearn_time = time.time()- t0
+
+print("[Scikit-Learn] Training time (s):  {0:.2f}".format(sklearn_time))
